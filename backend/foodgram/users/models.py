33fbb7d1244471625a2ba89bb9f656e939f4bbaf
talasov <talasov2017@yandex.ru>
2023-07-02 02:56:14 +0300

    commit

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -15,14 +15,6 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
-    class Meta:
-        ordering = ['id']
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
-    def __str__(self):
-        return self.username
-
 
 class Subscribe(models.Model):
     ''' Подписка на автора '''
